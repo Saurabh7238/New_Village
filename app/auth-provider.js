1ef931e5974249f018e392d86dd6302f7d64d5b8
Saurabh <saurabh705294@gmail.com>
2026-09-08 09:50:37 +0530

    Refactor Providers component to conditionally wrap children with SocketProvider based on pathname

--- app/auth-provider.js
+++ app/auth-provider.js
@@ -1,16 +1,18 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { usePathname } from "next/navigation";
 import { ThemeProvider } from "./theme-provider";
 import { SocketProvider } from "./socket-provider";
 
 export default function Providers({ children }) {
+  const pathname = usePathname();
+  const needsRealtime = pathname === "/dashboard/applications";
+
   return (
     <ThemeProvider>
       <SessionProvider>
-        <SocketProvider>
-          {children}
-        </SocketProvider>
+        {needsRealtime ? <SocketProvider>{children}</SocketProvider> : children}
       </SessionProvider>
     </ThemeProvider>
   );
